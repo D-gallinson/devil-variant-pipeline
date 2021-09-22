c30c4a14eab569a44e98a00ab877afba847d1f1d
Dylan Gallinson [dgallinson] <dgallinson@itn2.rc.usf.edu>
2021-09-22 18:11:53 -0400

    Removed bug that was pulling from NextSeq samples on host runs

--- 8_genomicsDB.sh
+++ 8_genomicsDB.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
-#SBATCH --job-name=8_genomicsDB_tumor
+#SBATCH --job-name=8_genomicsDB_host
 #SBATCH --partition=margres_2020
 #SBATCH --qos=margres20
 #SBATCH --mail-user=dgallinson@usf.edu
 #SBATCH --mail-type=END,FAIL
-#SBATCH --output=/work_bgfs/d/dgallinson/scripts/master/logs/Capture3/out/8_genomicsDB_tumor.out
-#SBATCH --error=/work_bgfs/d/dgallinson/scripts/master/logs/Capture3/err/8_genomicsDB_tumor.err
+#SBATCH --output=/work_bgfs/d/dgallinson/scripts/master/logs/Capture4/out/8_genomicsDB_host.out
+#SBATCH --error=/work_bgfs/d/dgallinson/scripts/master/logs/Capture4/err/8_genomicsDB_host.err
 #SBATCH --ntasks=1
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=3
-#SBATCH --mem=186G
+#SBATCH --mem=92G
 #SBATCH --time=21-00:00:00
 
 source main.env
 source refs.env
 source tools.env
 
-sample_type="tumor"  #Set to "host" or "tumor"
+sample_type="host"  #Set to "host" or "tumor"
 db_cmd="update"     #Set to "update" or "new"
 
 genomicsDB=$DATA/joint-variants/genomicsDB-${sample_type}
@@ -26,7 +26,6 @@ db_workspace="--genomicsdb-update-workspace-path"
 if [[ $sample_type == "host" ]]
 then
     samples=$DATA/gvcf/H*.g.vcf
-    samples=$WORK_BGFS/outputs/intermediates/7_gvcf/H*.g.vcf
 fi
 
 if [[ $db_cmd == "new" ]]
