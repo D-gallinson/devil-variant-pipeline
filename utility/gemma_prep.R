ff423f200170435e55d4f42bd9783e42d29aa40c
Dylan Gallinson [dgallinson] <dgallinson@itn2.rc.usf.edu>
2021-09-28 04:45:38 -0400

    Better CLI, option for test/training random splitting

--- utility/gemma_prep.R
+++ utility/gemma_prep.R
@@ -1,3 +1,6 @@
+library(argparse)
+
+
 inv_logit <- function(x){
   return(exp(x) / (1 + exp(x)))
 }
@@ -18,26 +21,37 @@ log_norm_undo <- function(x){
 }
 
 
-args <- commandArgs(trailingOnly=TRUE)
-pheno_input <- args[1]
-transform <- args[2]
+parser <- ArgumentParser()
+parser$add_argument("pheno_file", help="Path to the input phenotype file (this file will be overwritten by this script)")
+parser$add_argument("-t", "--transform", default="", help="Transform the input phenotype data with: inv_logit, logit, or log [default=None]")
+parser$add_argument("-x", "--xval", action="store_true", default=F, help="Xval mode to randomly subset the phenotype file to test/training [default=False]")
+parser$add_argument("-s", "--split", type="integer", default=20, help="Percentage to split into the testing set (e.g., -s 40 means 40% test, 60% training) [default=20]")
+args <- parser$parse_args()
 
-df <- read.csv(pheno_input, sep="\t")
+# These commands suck, try flags
+# args <- commandArgs(trailingOnly=TRUE)
+# pheno_input <- args[1]
+# transform <- args[2]
+# test_percent <- 20
+
+df <- read.csv(args$pheno_file, sep="\t")
 pheno <- df[,2]
 
-if(is.na(transform)){
-  if(transform == "inv_logit"){
+if(args$xval == F){
+  if(args$transform == "inv_logit"){
     pheno <- inv_logit(pheno)   
-  } else if(transform == "logit"){
+  } else if(args$transform == "logit"){
     pheno <- logit(pheno)
-  } else if(transform == "log"){
+  } else if(args$transform == "log"){
     pheno <- log(pheno)
   } else{
     print("!!!! Error in transform function, please use: inv_logit, logit, or log !!!!")
+    q()
   }
-  write.csv(pheno, pheno_input)
+  write.csv(pheno, args$pheno_file)
+}else{
+  test_percent <- round(nrow(df) * (args$split/100))
+  na_rows <- sample.int(nrow(df), test_percent)
+  df[na_rows, ] <- "NA"
+  write.csv(df, args$pheno_file)
 }
-
-print(pheno)
-print(log_norm_transform(pheno))
-print(log_norm_undo(log_norm_transform(pheno)))
