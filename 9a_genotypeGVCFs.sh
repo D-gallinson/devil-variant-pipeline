0c219bb014c6deb982c988f9b1b4dc6cfdbaaef8
D-gallinson <username9200@gmail.com>
2021-10-09 03:27:31 -0400

    Converted back to parallelization by chrom

--- 9a_genotypeGVCFs.sh
+++ 9a_genotypeGVCFs.sh
@@ -1,74 +1,32 @@
 #!/bin/bash
 #SBATCH --job-name=9a_GenotypeGVCFs-host_1
-#SBATCH --partition=muma_2021
-#SBATCH --qos=preempt_short
+#SBATCH --partition=margres_2020
+#SBATCH --qos=margres20
 #SBATCH --mail-user=dgallinson@usf.edu
-#SBATCH --mail-type=BEGIN,END,FAIL
-#SBATCH --output=/work_bgfs/d/dgallinson/scripts/master/logs/9a_genotypeGVCFs/host/out/1/%a.out
-#SBATCH --error=/work_bgfs/d/dgallinson/scripts/master/logs/9a_genotypeGVCFs/host/err/1/%a.err
+#SBATCH --mail-type=END,FAIL
+#SBATCH --output=/work_bgfs/d/dgallinson/scripts/master/logs/9a_genotypeGVCFs/host/out/3.out
+#SBATCH --error=/work_bgfs/d/dgallinson/scripts/master/logs/9a_genotypeGVCFs/host/err/3.err
 #SBATCH --ntasks=1
 #SBATCH --nodes=1
-#SBATCH --cpus-per-task=1
-#SBATCH --mem=10G
-#SBATCH --time=1-00:00:00
-#SBATCH --array=0-49
-
-#########################################################################
-# This script must point to directories indexed from 0 and corresponding
-# to the batch number (e.g., 0=batch 1). These directories are used as
-# multipliers to rename the err/out files properly upon completion
-# 
-# File names should then be changed using the following:
-# 
-# let batch_len=50
-# ext=err
-# for file in $(find -name *.err)
-# do
-#     file=${file:2}
-#     let mult=$(echo $file | grep -E -o '^[0-9]')
-#     let add=$(echo $file | grep -P -o '(?<=/)[0-9]')
-#     let num=$mult*$batch_len+$add
-#     mv $file final/$num.err
-# done
-#########################################################################
-
-
+#SBATCH --cpus-per-task=2
+#SBATCH --mem=187G
+#SBATCH --time=21-00:00:00
 
 source main.env
 source refs.env
 source tools.env
 
 sample_type="host"
-first_batch="false"
-progress=$SCRIPTS/logs/9a_genotypeGVCFs/$sample_type/progress.txt
-let start=0
+interval_num=3
 
-if [[ $first_batch == "true" ]]
-then
-    batch=1
-    end=$SLURM_ARRAY_TASK_MAX
-else
-    let start=$(cut -f3 $progress | tail -1)+1
-    let end=$SLURM_ARRAY_TASK_MAX+$start
-    let batch=$(cut -f1 $progress | tail -1)+1
-fi
-
-if [[ $SLURM_ARRAY_TASK_ID == $SLURM_ARRAY_TASK_MAX ]]
-then
-    let final=$SLURM_ARRAY_TASK_MAX+$start
-    echo -e "$batch\t$start\t$SLURM_ARRAY_TASK_MAX" >> $progress
-fi
-
-let file_num=$start+$SLURM_ARRAY_TASK_ID
-interval_file="$(printf "%04d" $file_num)-scattered.interval_list"
+interval=$INTERVAL_ARRAY/${interval_num}.list
 output=$DATA/joint-variants
-geno=${file_num}-${sample_type}.vcf
-intervals=${INTERVAL_ARRAY}/${interval_file}
+geno=${interval_num}-${sample_type}.vcf
 
-$GATK --java-options "-Xmx10G -Xms2G" GenotypeGVCFs \
+$GATK --java-options "-Xmx150G -Xms150G -XX:ParallelGCThreads=2" GenotypeGVCFs \
     --genomicsdb-shared-posixfs-optimizations true \
     --tmp-dir $output/tmp \
     -V gendb://$output/genomicsDB-${sample_type} \
     -R $REF \
-    -L $intervals \
+    -L $interval \
     -O $output/parallel/$geno
