72a84840251974412fd21eb5e1d3e7fc13b25886
Dylan Gallinson [dgallinson] <dgallinson@itn0.rc.usf.edu>
2021-10-13 03:44:16 -0400

    Most processing is now done without samples.py

--- utility/vcf_col_fix.py
+++ utility/vcf_col_fix.py
@@ -1,13 +1,33 @@
 from samples import Samples
 import argparse as ag
+import pandas as pd
+import re
 
 
 
 parser = ag.ArgumentParser(description="Helper to vcf_col_fix.sh")
 parser.add_argument("vcf_header", help="Path to the names of each sample from the VCF")
-parser.add_argument("rename_key", help="Path to the relevant rename_key.csv")
-parser.add_argument("tmp_dir", help="Path to the tmp dir")
+parser.add_argument("outpath", help="Output path for the tmp file")
 args = parser.parse_args()
 
+
 sample = Samples()
-sample.vcf_header_fix(args.vcf_header, args.rename_key, f"{args.tmp_dir}/vcf_fixed.txt")
+sample.subset("Tissue", "Tumour")
+haystack = sample.to_vcf_chip(inplace=False).dropna().to_list()
+
+target_list = pd.read_csv(args.vcf_header, header=None, squeeze=True)
+target_list[target_list == "T2-426081"] = "T4-426081" # This fixes a problem with the batch 1 tumors where the L0200 tumor was named as T2 instead of T4
+target_list = [target if re.match("T\d", target) else target[-6:] for target in target_list]
+
+corrected_names = []
+for target in target_list:
+	if re.match("T\d", target):
+		corrected_names.append(target)
+		continue
+	for mchip in haystack:
+		if re.match(f"T.*-{target}", mchip):
+			corrected_names.append(mchip)
+			break
+
+output = pd.Series(corrected_names)
+output.to_csv(args.outpath, header=False, index=False)
--- utility/vcf_col_fix.sh
+++ utility/vcf_col_fix.sh
@@ -3,7 +3,7 @@
 module load apps/python/3.8.5
 source main.env
 
-input=$RESULTS/filter/FINAL_SNPs-tumor.isec_raw.alleles.missing_100.vcf
+input=$RESULTS/filter/isec/FINAL_SNPs-tumor.isec_raw.vcf
 tmp_dir=../../tmp
 rename_key=/shares_bgfs/margres_lab/Devils/BEE_Probe_Data/Capture1_6-11-21/rename_key.csv
 log=${input/.vcf/.HEADER.log}
@@ -17,7 +17,7 @@ echo $line | cut -f2 -d: | sed 's/\s/\t/g' >> $log
 echo -e "\n" >> $log
 
 echo $samples | sed 's/\s/\n/g' > $tmp_dir/tmp_vcf_header.txt
-python $SCRIPTS/utility/vcf_col_fix.py $tmp_dir/tmp_vcf_header.txt $rename_key $tmp_dir
+python $SCRIPTS/utility/vcf_col_fix.py $tmp_dir/tmp_vcf_header.txt $tmp_dir/tmp_vcf_fixed.txt
 replacement=$(echo -e "#CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO\tFORMAT" $(cut -f 1 $tmp_dir/tmp_vcf_fixed.txt) | sed -r "s/\s/\t/g")
 
 echo "===== RENAMED VCF HEADER =====" >> $log
