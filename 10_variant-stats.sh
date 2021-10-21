237ad7a412908e64b5f80fc200ce03ad58112297
Dylan Gallinson <dgallinson@usf.edu>
2021-10-20 23:37:32 -0400

    README.txt auto-gen and check for INFO col

--- 10_variant-stats.sh
+++ 10_variant-stats.sh
@@ -4,8 +4,8 @@
 #SBATCH --qos=preempt_short
 #SBATCH --mail-user=dgallinson@usf.edu
 #SBATCH --mail-type=END,FAIL
-#SBATCH --output=/work_bgfs/d/dgallinson/outputs/results/vcf_stats/host/missing/run_log.out
-#SBATCH --error=/work_bgfs/d/dgallinson/outputs/results/vcf_stats/host/missing/run_log.err
+#SBATCH --output=/shares_bgfs/margres_lab/Devils/BEE_Probe_Data/scripts/master/logs/ATOMM/prelim/devil_survival/vcf_stats_FINAL_SNPs-tumor.subset.out
+#SBATCH --error=/shares_bgfs/margres_lab/Devils/BEE_Probe_Data/scripts/master/logs/ATOMM/prelim/devil_survival/vcf_stats_FINAL_SNPs-tumor.subset.err
 #SBATCH --ntasks=1
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=1
@@ -22,15 +22,27 @@ source main.env
 source refs.env
 source tools.env
 
-sample_type="host" #Change to "tumor" or "host"
-input_dir="${RESULTS}/filter"    #Change to relevant input folder
-input_file="FINAL_SNPs-host.minDP_10.maxDP_100.alleles.missing_100.mac_2.vcf.gz" #Change to relevant input file
-output_folder="missing"  #Change to relevant output folder (assumed to be in $WORK_BGFS/outputs/results/vcf_stats)
+sample_type="tumor" #Change to "tumor" or "host"
+input_dir="${RESULTS}/filter/ATOMM"    #Change to relevant input folder
+input_file="FINAL_SNPs-tumor.isec_raw.subset.missing_100.vcf.gz" #Change to relevant input file
+output_folder="ATOMM/subset"  #Change to relevant output folder (assumed to be in $$RESULTS/vcf_stats)
 
-id=${sample_type}_${output_folder}
+fname=$(echo $output_folder | sed 's/.*\///g')
+id=${sample_type}_${fname}
 input="${input_dir}/${input_file}"
 output="${RESULTS}/vcf_stats/${sample_type}/${output_folder}"
 
+# Check to ensure that the VCF has a non-empty INFO column
+info_check=$(zgrep -P -m1 '^(?!#)' $input | cut -f8)
+if [[ $info_check == "." ]]
+then
+    echo "!!!!! INFO COLUMN IS EMPTY !!!!!"
+    echo "Please re-run VCFTools with the \"--recode-INFO-all flag\""
+    echo "Exiting"
+    exit 1
+fi
+
+# Generate output folders
 mkdir -p $output/data
 mkdir -p $output/tables
 mkdir -p $output/plots
@@ -117,6 +129,31 @@ Rscript utility/vcf_metrics.R $id $output
 
 # Generate plots
 Rscript utility/vcf_plot.R $id $output
+
+# Generate the README file describing each file made from this script
+echo "===== SUMMARY OF FILES GENERATED =====
+data/
+    $fname.INFO     Values from the INFO column of a VCF
+    $fname.sample   Per-sample VCF statistics
+    $fname.site     Per-site VCF statistics
+    $fname.snpden   Number of SNPs within 125kb intervals spanning the genome
+plots/
+    info_plots.$fname.pdf           Plots generated from the INFO column of a VCF    
+    karyotype_density.$fname.pdf    Density plots over the devil autosomes
+    sample_plots.$fname.pdf         Plots generated from statistics generated per-sample
+    site_plots.$fname.pdf           Plots generated from statistics generated per-site
+    snp_density.$fname.pdf          A histogram of SNPs contained within 125kb intervals
+tables/
+    all_chroms.bp.$fname.snpden      Statistics regarding SNP distances and the number of SNPs per chromosome (SNP_count). Generated over all chromosomes
+    big_chroms.bp.$fname.snpden      Statistics regarding SNP distances and the number of SNPs per chromosome (SNP_count). Generated over large chromosomes (1-6 and X)
+    bottom_mac.$fname.csv            Table containing the number of sites with a minor allele count (MAC) of 0-10
+    info_summary.$fname.csv          Statistics generated from the VCF INFO column
+    little_chroms.bp.$fname.snpden   Statistics regarding SNP distances and the number of SNPs per chromosome (SNP_count). Generated over small chromosomes (Y and unplaced scaffolds)
+    per_chrom.bp.$fname.csv          Mean SNP distances and the number of SNPs per chromosome (SNP_count). Generated for all chromosomes on a per-chromosome basis
+    sample_summary.$fname.csv        Statistics generated per-sample
+    site_summary.$fname.csv          Statistics generated per-site
+    SNP_density.$fname.csv           Counts of the 125kb intervals containing n SNPs
+" > $output/README.txt
 
 # Remove tmp files
 rm $output/tmp*
