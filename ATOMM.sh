76f8bf4e5cb3388d16efcfdc21fa24b02667672e
Dylan Gallinson <dgallinson@usf.edu>
2021-10-20 23:51:34 -0400

    Better marginal stats output

--- ATOMM.sh
+++ ATOMM.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#SBATCH --job-name=ATOMM-marginal
+#SBATCH --job-name=ATOMM-convergence
 #SBATCH --partition=muma_2021
 #SBATCH --qos=preempt_short
 #SBATCH --mail-user=dgallinson@usf.edu
 #SBATCH --mail-type=END,FAIL
-#SBATCH --output=/work_bgfs/d/dgallinson/scripts/master/logs/ATOMM/prelim/devil_survival/rand_seq_estimate.out
-#SBATCH --error=/work_bgfs/d/dgallinson/scripts/master/logs/ATOMM/prelim/devil_survival/rand_seq_estimate.err
+#SBATCH --output=/shares_bgfs/margres_lab/Devils/BEE_Probe_Data/scripts/master/logs/ATOMM/prelim/devil_survival/subset_filter_marginal.out
+#SBATCH --error=/shares_bgfs/margres_lab/Devils/BEE_Probe_Data/scripts/master/logs/ATOMM/prelim/devil_survival/subset_filter_marginal.err
 #SBATCH --ntasks=1
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=24
@@ -16,17 +16,21 @@ module purge
 module add apps/matlab
 
 # This must be run from the ATOMM/ directory
-matlab -nodisplay -nosplash -r "devil_marginal"
+# matlab -nodisplay -nosplash -r "devil_marginal"
 
 r=''
 r+='host <- read.table("output/marginal_host.txt"); patho <- read.table("output/marginal_pathogen.txt"); '
 r+='sink("output/MARGINAL_STATS.txt"); '
-r+='cat(paste("Host NAs:", sum(is.na(host$V4)), "\n")); cat(paste("Pathogen NAs:", sum(is.na(patho$V4)), "\n\n")); '
+r+='cat("Host NAs:", sum(is.na(host$V4)), "\n"); cat("Pathogen NAs:", sum(is.na(patho$V4)), "\n\n"); '
 r+='host <- host[complete.cases(host),]; patho <- patho[complete.cases(patho),]; '
+r+='cat("===== Host p-values =====\n"); cat("Total host SNPs:", nrow(host), "\n"); '
+r+='summary(host$V4); cat("\n"); '
 r+='host_sig <- host[host$V4 < 0.05,]; patho_sig <- patho[patho$V4 < 0.05,]; '
-r+='cat(paste("Host SNPs (p < 0.05):", dim(host_sig)[1], "\n")); '
+r+='cat("Host SNPs (p < 0.05): ", nrow(host_sig), " (", round((nrow(host_sig) / nrow(host)) * 100, 2), "%)", "\n", sep=""); '
 r+='summary(host_sig$V4); '
 r+='cat("\n"); '
-r+='cat(paste("Pathogen SNPs (p < 0.05):", dim(patho_sig)[1], "\n")); '
+r+='cat("===== Pathogen p-values =====\n"); cat("Total pathogen SNPs:", nrow(patho), "\n"); '
+r+='summary(patho$V4); cat("\n"); '
+r+='cat("Pathogen SNPs (p < 0.05):", nrow(patho_sig), " (", round((nrow(patho_sig) / nrow(patho)) * 100, 2), "%)", "\n", sep=""); '
 r+='summary(patho_sig$V4); '
 Rscript -e "$r"
