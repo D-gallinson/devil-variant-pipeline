cc85f02f77f6d588e58a1f84be3815202b419c8a
Dylan Gallinson <dgallinson@usf.edu>
2021-12-26 14:51:16 -0500

    Fixed a bug with CIs in violin plots

--- utility/gemma_stats.R
+++ utility/gemma_stats.R
@@ -1,6 +1,7 @@
 library(ggplot2)
 library(rstan)
 library(stringr)
+library(reshape2)
 
 downsample <- function(df, samples=-1){
   N <- nrow(df)
@@ -23,6 +24,20 @@ to_sim_mat <- function(df, chains=1){
   arr <- array(unlist(df), c(nrow(df), chains, ncol(df)),
                dimnames=list(NULL, NULL, colnames(df)))
   return(arr)
+}
+
+to_CI <- function(x){
+  return(quantile(x, c(0.025, 0.975)))
+}
+
+violin <- function(df, color, ylab, ylim=c(0, NA)){
+  ggplot(df, aes(x=variable, y=value)) +
+    geom_violin(fill=color) +
+    stat_summary(fun=mean, geom="point", size=2, color="black") +
+    stat_summary(fun=to_CI, geom="line", size=0.25, color="black") +
+    ylim(ylim) +
+    ylab(ylab) +
+    theme(axis.title.x=element_blank())
 }
 
 # Read in input/output CLI args
@@ -77,10 +92,10 @@ for(param in model_params){
   }else if(param %in% log_cols && transform){
     y_label <- paste("log(", param, ")", sep="")
   }
-
+  
   plot <- ggplot(gemma_hyp, aes_string(x="iter", y=param)) + 
-                   geom_point() +
-                   ggtitle(paste("GEMMA Hyperparameter:", param)) + labs(x="Iter", y=y_label)
+    geom_point() +
+    ggtitle(paste("GEMMA Hyperparameter:", param)) + labs(x="Iter", y=y_label)
   print(plot)
   
   if(param %in% c("pve", "pge", "n_gamma")){
@@ -95,3 +110,15 @@ for(param in model_params){
 dev.off()
 write.csv(stat_df, paste(output, "stats.csv", sep="."))
 write.csv(fit_stats, paste(output, "convergence_stats.csv", sep="."))
+
+# Generate PVE and PGE violin plots
+violin_df <- read.csv(input, sep="\t")
+violin_df <- violin_df[, c("pve", "pge")]
+colnames(violin_df) <- c("PVE", "PGE")
+violin_df <- melt(violin_df)
+
+plot <- violin(violin_df, "skyblue3", "Proportion Variance Explained")
+ggsave(
+  paste(output, "violin_plots.png", sep="."),
+  plot=plot
+)
